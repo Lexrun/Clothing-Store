a9c2e31ffee550c22d4673066dbd2ee33b871a69
Alex <61022995+Lexrun@users.noreply.github.com>
2022-04-18 19:30:09 +0300

    Update Cashier.java

--- ClothingStore/src/com/company/Cashier.java
+++ ClothingStore/src/com/company/Cashier.java
@@ -1,6 +1,4 @@
 package com.company;
-import com.company.Product;
-import com.company.Main;
 import java.time.LocalDate;
 import java.util.List;
 import java.text.DecimalFormat;
@@ -13,27 +11,27 @@ public class Cashier {
     public double shoesdiscount;
     public double shirtsdiscount;
     public double WEDNESDAYDICOUNT;
-    String Tuesday = "WEDNESDAY";
+    String Wednesday = "WEDNESDAY";
     DecimalFormat df = new DecimalFormat("#.00");
     public Cashier(List<Product> cart,LocalDate localdate)
     {
-        for(Product n : cart)
+        for(Product product : cart)
         {
-            System.out.println(n.getName() + " - " + n.getBrand());
-            System.out.println(n.getPrice() + "$");
-            subtotal += n.getPrice();
-            if (localdate.getDayOfWeek().toString() == Tuesday) {
-                if (n.getClass() == Shoes.class) {
-                    shoesdiscount = n.getPrice() * 0.25;
+            System.out.println(product.getName() + " - " + product.getBrand());
+            System.out.println(product.getPrice() + "$");
+            subtotal += product.getPrice();
+            if (localdate.getDayOfWeek().toString() == Wednesday) {
+                if (product.getClass() == Shoes.class) {
+                    shoesdiscount = product.getPrice() * 0.25;
                     System.out.println("#discount 25% - " + df.format(shoesdiscount) + "$");
                 }
-                if (n.getClass() == Shirt.class) {
-                    shirtsdiscount = n.getPrice() * 0.10;
+                if (product.getClass() == Shirt.class) {
+                    shirtsdiscount = product.getPrice() * 0.10;
                     System.out.println("#discount 25% - " + df.format(shirtsdiscount) + "$");
                 }
             }
                     if (cart.size() >= 3) {
-                        discount =  n.getPrice() * 0.20;
+                        discount =  product.getPrice() * 0.20;
                         System.out.println("#discount 20% - " + df.format(discount) + "$" );
                     }
                     DISCOUNT += discount;
@@ -41,9 +39,9 @@ public class Cashier {
             WEDNESDAYDICOUNT = shirtsdiscount + shoesdiscount;
             DISCOUNT = DISCOUNT + WEDNESDAYDICOUNT;
             System.out.println("-----------------------------------------------------------------------------------");
-            System.out.println("SUBTOTAL:" + df.format(subtotal) + "$");
+            System.out.println("SUBTOTAL:" + df.format(DISCOUNT + subtotal)  + "$");
             System.out.println("DISCOUNT:" + df.format(DISCOUNT)  + "$\n");
-            System.out.println("TOTAL:" + df.format(DISCOUNT + subtotal)  + "$");
+            System.out.println("TOTAL:" + df.format(subtotal) + "$");
 
 }
 }
